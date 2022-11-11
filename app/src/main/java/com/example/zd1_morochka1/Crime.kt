1999fba7a8e6f4d5cbf80827291c267104ac8f5a
ural-ECTS <ural@ects.ru>
2022-11-11 13:13:46 +0500

    first commit

--- app/src/main/java/com/example/zd1_morochka1/Crime.kt
+++ app/src/main/java/com/example/zd1_morochka1/Crime.kt
@@ -6,12 +6,10 @@ data class Crime(var id: UUID = UUID.randomUUID()) {
     var title: String = ""
     var date: Int = 0
     var isSolved: Boolean = false
-    constructor(id: UUID, ititle: String, date: Int, isSolved: Boolean) : this(id){
+    constructor(id: UUID, title: String, date: Int, isSolved: Boolean) : this(id){
         this.title = title
         this.date = date
         this.id = id
         this.isSolved = isSolved
     }
-
-
 }
