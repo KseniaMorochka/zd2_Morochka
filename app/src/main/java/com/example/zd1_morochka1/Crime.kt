b71583ed95ab782d5672af433ba86e314c26729b
ural-ECTS <ural@ects.ru>
2022-11-11 12:12:23 +0500

    finish

---
+++ app/src/main/java/com/example/zd1_morochka1/Crime.kt
@@ -0,0 +1,17 @@
+package com.example.zd1_morochka1
+
+import java.util.*
+
+data class Crime(var id: UUID = UUID.randomUUID()) {
+    var title: String = ""
+    var date: Int = 0
+    var isSolved: Boolean = false
+    constructor(id: UUID, ititle: String, date: Int, isSolved: Boolean) : this(id){
+        this.title = title
+        this.date = date
+        this.id = id
+        this.isSolved = isSolved
+    }
+
+
+}
